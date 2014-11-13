61b4b07f1d2846de4d250aa087e2a44b8cc10c84
Workstation <nicolaorford5@gmail.com>
2014-11-13 16:49:03 +0200

    Final solution

--- Problem_E/rockpaperscissors.py
+++ Problem_E/rockpaperscissors.py
@@ -1,20 +1,44 @@
 import random
+tally = []
 def rockpaperscissors():
-    print "Welcome to rock paper scissors"
-    print "What is your move?"
-    answer = raw_input("Type r, p or s OR R, P or S and hit 'Enter':")
-    if answer == "r" or answer == "R" or answer == "p" or answer =="P" or answer == "s" or answer == "S":
-        print "This is a legal move."
+    print "What is your move?",
+    answer = raw_input("Type r, p or s and hit 'Enter':")
+    if answer == "r" or answer == "p" or answer == "s":
+        
 
 	computerchoices = ['r','p','s']
 	pcchoice = random.choice(computerchoices)
 	print pcchoice
 
 
+	if answer == pcchoice:
+		print "Draw"
+		tally.append('d')
+	elif (answer == 'r' and pcchoice == 'p') or \
+             (answer == 'p' and pcchoice == 's') or \
+	     (answer == 's' and pcchoice == 'r'):
+		print "You wins"
+		tally.append('w')
+	else:
+		print "Computer wins"
+		tally.append('l')
 
 	rockpaperscissors()
-    else:
-        print "That's not fair. Tournament over"
-        
 
+    else:
+	print "That's not fair. Tournament over"
+	numwins = tally.count('w')
+	numlosses = tally.count('l')
+	if numwins > numlosses:
+		print "You win"
+	elif numwins < numlosses:
+		print "You lose!!!"
+	else:
+	print "We tie"
+        
+	print "you won {} times".format(tally.count('w'))
+	print "you lost {} times".format(tally.count('l'))
+	print "we drew {} times".format(tally.count('d'))
+        
+print "Welcome to rock paper scissors"
 rockpaperscissors()
